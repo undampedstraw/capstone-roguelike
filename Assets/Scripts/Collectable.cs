3b056815f74d871deee54c28f6cf0a6f3de1c4d8
yfu03 <yannisfu@gmail.com>
2024-10-22 17:54:56 -0500

    remove duplicate script

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Collectible : Collidable
+public class Collectable : Collidable
 {
     protected bool collected;
 
